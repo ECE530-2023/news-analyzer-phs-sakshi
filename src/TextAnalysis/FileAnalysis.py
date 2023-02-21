5bd5d799a29f4f4c450b04992908dae08226f215
Sakshi Sharma <b417037@iiit-bh.ac.in>
2023-02-21 00:50:14 -0500

    File uploader (#1)
    * Stub code - file uploader

--- src/TextAnalysis/FileAnalysis.py
+++ src/TextAnalysis/FileAnalysis.py
@@ -12,8 +12,35 @@ app = Flask(__name__)
 def get_paragraphs_by_keywords():
     return '', 200
 
+# @Input parameters - sentiment to match the paragraphs
+# Response -
+# 200 - Successful - list of paragraphs
+# 400 - Bad Request
+# 500 - Internal Server Error
 @app.route('/paragraphsBySentiment', methods=['POST'])
 def get_paragraphs_by_sentiment():
     return [], 200
 
-def get_
+# @Input parameters - keywords to
+# Response -
+# 200 - Successful - definition of the keyword
+# 400 - Bad Request - keyword not found
+# 500 - Internal Server Error
+@app.route('/keywordDefinition',methods=['GET'])
+def get_keyword_definition():
+    definition = get_keyword_definition(keyword)
+    if definition:
+        return definition, 200
+    return 'Error - Keyword not found', 400
+
+# @Input parameters - file id of the file to summarize
+# Response -
+# 200 - Successful - summary of the file
+# 400 - Bad Request - file not found
+# 500 - Internal Server Error
+@app.route('/documentSummary',methods=['GET'])
+def get_document_summary():
+    summary = get_document_summary(file)
+    if summary:
+        return summary, 200
+    return 'Unable to find document', 400
--- src/TextAnalysis/TextAnalyzerImpl.py
+++ src/TextAnalysis/TextAnalyzerImpl.py
@@ -18,3 +18,11 @@ def convert_file_to_text(file):
 
 def tag_document_by_keyword(file):
     return True
+
+def get_definition(keyword):
+    words={'sun':'sun','moon':'moon'}
+    return words[keyword] if keyword in words else None
+
+def get_document_summary(file_id):
+    summary={1:'doc 1',2:'doc 2'}
+    return summary[file_id] if file_id in summary else None
