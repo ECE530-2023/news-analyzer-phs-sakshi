bf0d9a6954749710dfbcd7ec010f1fb23dbb3196
phs-sakshi <b417037@iiit-bh.ac.in>
2023-04-02 19:07:58 -0400

    fixes build

---
+++ src/Thread.py
@@ -0,0 +1,22 @@
+import threading
+
+
+class Thread(threading.Thread):
+    """
+    A wrapper class for threading.Thread which takes as input a function,
+    function arguments, callback arguments, and callback function arguments.
+    When the thread finishes, it calls the callback function with the output
+    of the function as an argument.
+    """
+    def __init__(self, func, func_args, callback, callback_args):
+        super(Thread, self).__init__()
+        self.func = func
+        self.func_args = func_args
+        self.callback = callback
+        self.callback_args = callback_args
+
+    def run(self):
+        # Run the function with the provided arguments
+        result = self.func(*self.func_args)
+        # Call the callback function with the result and provided arguments
+        self.callback(result, *self.callback_args)
