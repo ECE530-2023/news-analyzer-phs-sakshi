f36d8ec759f9abea975585c3f8c5923d3eb2799f
phs-sakshi <b417037@iiit-bh.ac.in>
2023-05-05 11:49:13 -0400

    fix unit test

--- tests/unittests/file_uploader_test.py
+++ tests/unittests/file_uploader_test.py
@@ -69,8 +69,8 @@ class TestFileUpload(unittest.TestCase):
         with app.test_client() as client:
             response = client.get('/download?fileId=file_id', headers={'Authorization': 'Bearer invalid_token'})
 
-        # Check that the response status code is 404
-        self.assertEqual(response.status_code, 404)
+        # Check that the response status code is 500
+        self.assertEqual(response.status_code, 500)
 
     def test_download_document_file_not_found(self):
         # Mock a GET request with a non-existent fileId and a valid token in the headers
@@ -78,8 +78,8 @@ class TestFileUpload(unittest.TestCase):
             response = client.get('/download?fileId=invalid_file_id',
                                   headers={'Authorization': 'Bearer token'})
 
-        # Check that the response status code is 404
-        self.assertEqual(response.status_code, 404)
+        # Check that the response status code is 500
+        self.assertEqual(response.status_code, 500)
 
 
 if __name__ == '__main__':
